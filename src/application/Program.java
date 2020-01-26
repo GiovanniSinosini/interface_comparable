a16f335a52555d1c823402e723cbd14ec41367e4
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-26 23:10:34 +0000

    Comparable interface implementation

---
+++ src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import entities.Employee;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		List<Employee> list = new ArrayList<>();
+		
+		String path = "C:\\temp\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");  // split the string in two
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));  // save the split string to list
+				employeeCsv = br.readLine();
+			}
+			
+			Collections.sort(list);   // way to order
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", " + emp.getSalary());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
